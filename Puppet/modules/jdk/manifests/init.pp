8e766d5668516a0e4f4359a14e1253d598e957b7
Luis Motta Campos <lucampos@ebay.com>
2014-07-23 22:58:07 +0100

    Added parametrisation to the jdk class

--- Puppet/modules/jdk/manifests/init.pp
+++ Puppet/modules/jdk/manifests/init.pp
@@ -1,12 +1,14 @@
 # class jdk - installs Oracle JDK 1.7.0_65 from Oracle's tarball.
-class jdk {
+class jdk (
+  $java_home = '/usr/java',
+  $java_tarball = '/vagrant/Repository/jdk-7u65-linux-x64.tar.gz'
+) {
 
-  # TODO: parametrise the java deployment dir
-  # TODO: parametrise the java tarball location
-  # TODO: register the java binaries with the alternatives system
+  # TODO: register the java binaries with the alternatives system?
+  # TODO: Add the java binaries to the path using the "/etc/profile.d/" directory?
 
   file {
-    '/var/lib/java':
+    $java_home:
       ensure => directory,
       mode   => '0755',
       owner  => 'root',
@@ -15,9 +17,9 @@ class jdk {
 
   # Deploy Java from tarball
   exec { 'unpack-oracle-jdk':
-    command => 'tar xzCf /var/lib/java /vagrant/Repository/jdk-7u65-linux-x64.tar.gz',
+    command => "tar xzCf ${java_home} ${java_tarball}",
     path    => ['/bin', '/usr/bin'],
-    require => File['/var/lib/java'],
-    onlyif  => '/usr/bin/test \'! -d /var/lib/java/jdk1.7.0_65\''
+    require => File[$java_home],
+    onlyif  => "/usr/bin/test '! -d ${java_home}/jdk1.7.0_65'"
   }
 }
